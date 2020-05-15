6ef984b61f284c8ddc27b4b473fb3b8c8a1bc0e0
Abiu12 <62127374+Abiu12@users.noreply.github.com>
2020-05-15 18:40:04 -0500

    Update E1.java

--- E1.java
+++ E1.java
@@ -4,21 +4,26 @@
 *@version 19/04/2020
  */
 import java.util.Scanner;// se importa el Scanner para poder leer el numero desde el teclado
-public class E1{
-public static void main(String []args){
+public  class E1{
+
        Scanner sc=new Scanner(System.in);//SE crea la variable del Scanner
         int a=sc.nextInt();//Se lee el numero
         
         String serie="";// Se declara el string vacio para despues poder agregarle los numeros de la serie
         int b=0;// Se declara una variable auxiliar para poder realizar la serie
-        serie= ""+a;// Se le agrega el numero igresado a la serie
+       
+        public   String Serie(){
+        serie= ""+a;// Se le agrega el numero ingresado a la serie
         
-        if(a%2!=0&&a>0){//condicion para el caso que sea u n numero impar
+        if(a%2!=0&&a>0){//condicion para el caso que sea un numero impar
+            b=a;
             a++;
+            a=a+b;
+            a=a+b;
         serie=serie+","+a;}
         while(a>1){// ciclo para realizar la serie, en donde se rechazan los numeros negativos
             b=a;
-    if(a%2==0){//condicion para los numeros que tienen mitad
+        if(a%2==0){//condicion para los numeros que tienen mitad
         a=a/2;
         serie=serie+","+a;
           }
@@ -29,7 +34,7 @@ public static void main(String []args){
         }
             
         
-     System.out.println(serie);//Se imprime el String que contiene la serie
+     return serie;//Se retorna el String que contiene la serie
 
 
 }
